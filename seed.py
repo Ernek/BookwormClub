3ebabf3166252d6258f7a86dacc671d9c356b565
ernesto <ebaezcheco@gmail.com>
2024-12-19 10:42:13 -0800

    fixing trailing linkages that are not being used

--- seed.py
+++ seed.py
@@ -14,8 +14,6 @@ with open('generator/bookclubusers.csv') as users:
 with open('generator/books.csv') as books:
     db.session.bulk_insert_mappings(Book, DictReader(books))
 
-# db.session.commit()
-
 with open('generator/reads.csv') as reads:
     db.session.bulk_insert_mappings(Read, DictReader(reads))
 
